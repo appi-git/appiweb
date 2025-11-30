c602633ba34100e6c59683048a6c005f786cbc4b
appi <75108610+appi-git@users.noreply.github.com>
2025-11-30 15:10:26 +0530

    Create journal.js

---
+++ journal.js
@@ -0,0 +1,7 @@
+let userName = prompt("Please enter your name:");
+    if (userName !== null) {
+        alert("Hello, " + userName + "!");
+    } else {
+        alert("User canceled the prompt.");
+    }
+    document.write("hello " + userName)
