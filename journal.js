1c98882a8d280e60ed9e00756c9d7add4ee7629a
appi <75108610+appi-git@users.noreply.github.com>
2025-11-30 15:20:02 +0530

    Update journal.js

--- journal.js
+++
@@ -1,6 +0,0 @@
-let userName = prompt("Please enter your name:");
-    if (userName !== null) {
-        alert("Hello, " + userName + "!");
-    } else {
-        alert("User canceled the prompt.");
-    }
